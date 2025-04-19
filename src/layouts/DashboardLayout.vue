aa9db776d59030e2a5b1d4d28285d61ec1d6b1fd
Jonathan Guaynora <guaynorajon@gmail.com>
2025-04-19 18:31:54 -0500

    added layouts

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MainLayout from '@/layouts/MainLayout.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const title = (route.meta.title as string) || 'Dashboard'
+</script>
+
+<template>
+  <MainLayout :title="title">
+    <router-view />
+  </MainLayout>
+</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import BaseSidebar from '@/components/BaseSidebar.vue'
+import AppBar from '@/components/AppBar.vue'
+
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <BaseSidebar />
+
+    <div class="flex flex-col flex-1 overflow-hidden">
+      <AppBar :title="title" />
+      <main class="flex-1 overflow-y-auto p-6 bg-gray-50">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
